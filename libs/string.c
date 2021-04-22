ea9e14f42ba4b68f6797e130c20d9796308ee788
penpenf28 <penpenf28@163.com>
2021-04-22 19:37:38 +0800

    part string  add memset && memcpy && memcmp

--- libs/string.c
+++ libs/string.c
@@ -24,3 +24,50 @@ size_t strnlen(const char *s, size_t len){
 }
 
 
+
+void *
+memset(void *s,char c,size_t n){
+#ifdef __HAVE_ARCH_MEMSET
+    return __memset(s,c,n);
+#else
+    char *p=s;
+    while (n-- >0)
+    {
+        /* code */
+        *p++=c;
+    }
+    
+#endif //__HAVE_ARCH_MEMSET
+}
+
+
+//string copy or memcopy
+void *
+memcpy(void* des,const void *src,size_t n){
+#ifdef __HAVE_ARCH_MEMCPY
+    return __memcpy(des,src,n);
+#else
+    const char *s = src;
+    char *d = des;
+    while(n-->0){
+        *d++ = *s++;
+    }
+    return des;
+#endif //__HAVE_ARCH_MEMCPY
+}
+
+
+//compare two blocks of memory
+int 
+memcmp(const void *v1,const void *v2,size_t n){
+    const char *s1 = (const char *)v1;
+    const char *s2 = (const char *)v2;
+    while(n-->0){
+        if(*s1!=*s2){
+            return (int)((unsigned char)*s1 - (unsigned char)*s2);
+        }
+        s1++;s2++;
+    }
+    return 0;
+} 
+
